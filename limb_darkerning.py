8cac26e009e51f2e41efe15ec5478dc4a6c741d1
Lokesh Manickavasaham <manick.lokee@gmail.com>
2024-11-08 23:18:07 +0530

    Update limb_darkerning.py

--- limb_darkerning.py
+++ limb_darkerning.py
@@ -1,16 +1,21 @@
-import math 
+"""
 
-# Limb darkening coefficients of non linear law (c1, c2, c3, c4)
-ldc = [0.3369471782720396,
-  0.20582268551248764,
-  0.41483350298449173,
-  -0.25608656100515886]
+Requried Paramters
+
+ldc (array) --> limb darkening coefficients of non-linear law (4)
+
+"""
+
+import math
+
+def quadratic_limb_darkening(ldc, method):
+  if method == 'quadratic':
+      u1 = ((12/35)*ldc[0]) + ldc[1] + ((164/105)*ldc[2]) + (2*ldc[3])
+      u2 = ((10/21)*ldc[0]) - ((34/63)*ldc[2]) - ldc[3]
+      return u1, u2
+  else:
+      print("Invalid method")
 
 method='quadratic'
 
-if method == 'quadratic':
-    u1 = ((12/35)*ldc[0]) + ldc[1] + ((164/105)*ldc[2]) + (2*ldc[3])
-    u2 = ((10/21)*ldc[0]) - ((34/63)*ldc[2]) - ldc[3]
-    print(f"u1 = {u1:.5g}, u2 = {u2:.5g}")
-else:
-    print("Invalid method")
+u1, u2 = quadratic_limb_darkening(ldc, method)
